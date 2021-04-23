6a893fdd39acef333a124b399a2ac3dde8524482
apknusel <apknusel@stu.naperville203.org>
2021-04-23 11:51:21 -0500

    Finished Prototype
    Finished the basic working prototype of the game.

---
+++ 2D Platformer Game/Assets/Scripts/levelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelManager : MonoBehaviour
+{
+    public void restart()
+    {
+        SceneManager.LoadScene("Start Screen");
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ 2D Platformer Game/Assets/Scripts/smoothCameraFollow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smoothCameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float timeOffset;
+    public Vector2 posOffset;
+    private Vector3 velocity;
+
+    public float leftLimit;
+    public float rightLimit;
+    public float bottomLimit;
+    public float topLimit;
+
+    void Update()
+    {
+        Vector3 startPos = transform.position;
+        Vector3 endPos = player.transform.position;
+        endPos.x += posOffset.x;
+        endPos.y += posOffset.y;
+        endPos.z = -10;
+        transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
+
+        transform.position = new Vector3
+        (
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+            transform.position.z);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector2(leftLimit, topLimit), new Vector2(rightLimit, topLimit));
+        Gizmos.DrawLine(new Vector2(rightLimit, topLimit), new Vector2(rightLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(rightLimit, bottomLimit), new Vector2(leftLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(leftLimit, topLimit));
+    }
+}

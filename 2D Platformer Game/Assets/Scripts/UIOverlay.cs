7443ad405cf9d97362dd75229f86f992f54877c2
apknusel <apknusel@stu.naperville203.org>
2021-05-11 08:59:46 -0500

    Added PowerUps

---
+++ 2D Platformer Game/Assets/Scripts/UIOverlay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIOverlay : MonoBehaviour
+{
+    public Text ammoDisplay;
+
+    public void updateAmmo(int ammoCount)
+    {
+        ammoDisplay.text = ammoCount.ToString();
+    }
+}
--- 2D Platformer Game/Assets/Scripts/playerController.cs
+++ 2D Platformer Game/Assets/Scripts/playerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerController : MonoBehaviour
 {
@@ -11,11 +12,20 @@ public class playerController : MonoBehaviour
     public Transform crouchFirePoint;
     public GameObject bulletPrefab;
 
+    public int ammoCount = 10;
+    private int tempAmmo;
+    public  UIOverlay overlay;
+
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
     bool jump = false;
     bool crouch = false;
+
+    void Start()
+    {
+        overlay.updateAmmo(ammoCount);
+    }
 
     void Update()
     {
@@ -37,8 +47,10 @@ public class playerController : MonoBehaviour
             crouch = false;
         }
 
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && ammoCount != 0)
         {
+            ammoCount -= 1;
+            overlay.updateAmmo(ammoCount);
             if (crouch == true)
             {
                 crouchShoot();
@@ -75,5 +87,29 @@ public class playerController : MonoBehaviour
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            SceneManager.LoadScene("Death Screen");
+        }
+
+        if (collision.gameObject.tag == "infiniteammo")
+        {
+            StartCoroutine(AmmoPowerup());
+            Destroy(collision.gameObject);
+        }
+    }
+
+    IEnumerator AmmoPowerup()
+    {
+        tempAmmo = ammoCount;
+        ammoCount = -1;
+        overlay.updateAmmo(ammoCount);
+        yield return new WaitForSeconds(5);
+        overlay.updateAmmo(tempAmmo);
+        ammoCount = tempAmmo;
     }
 }

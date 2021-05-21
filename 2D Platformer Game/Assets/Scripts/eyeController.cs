7a2fef2aae73361cd5ffc7eb9eeac6ff8928c9a0
apknusel <apknusel@stu.naperville203.org>
2021-05-21 09:41:35 -0500

    Fixed some bugs and finished boss level

---
+++ 2D Platformer Game/Assets/Scripts/eyeController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class eyeController : MonoBehaviour
+{
+    public Vector3 startPosition;
+    public Vector3[] moveToPoints;
+    public Vector3 currentPoint;
+
+    public float moveSpeed;
+
+    public int pointSelection;
+
+    public Transform target;
+    public Transform shootPoint;
+    public GameObject bullet;
+    public float fireRate = 3000f;
+    public float shootingPower = 20f;
+
+    public Animator animator;
+
+    private float shootingTime;
+
+    public int health = 100;
+
+    void Start()
+    {
+        this.transform.position = startPosition;
+    }
+
+    private void Update()
+    {
+        Fire();
+        Move();
+    }
+
+    private void Fire()
+    {
+        if (Time.time > shootingTime && Time.time >= 3)
+        {
+            animator.Play("shoot", 0, 0);
+            shootingTime = Time.time + fireRate / 1000;
+            Vector2 myPos = new Vector2(shootPoint.position.x, shootPoint.position.y);
+            GameObject projectile = Instantiate(bullet, myPos, Quaternion.identity);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("bullet"))
+        {
+            health -= 5;
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+                SceneManager.LoadScene("Win Screen");
+            }
+        }
+    }
+
+    void Move()
+    {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, currentPoint, Time.deltaTime * moveSpeed);
+        if (this.transform.position == currentPoint)
+        {
+            pointSelection++;
+            if (pointSelection == moveToPoints.Length)
+            {
+                pointSelection = 0;
+            }
+            currentPoint = moveToPoints[pointSelection];
+        }
+    }
+}
---
+++ 2D Platformer Game/Assets/Scripts/homingBullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class homingBullet : MonoBehaviour
+{
+    public GameObject target;
+
+    public float speed = 5f;
+    public float rotateSpeed = 200f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 direction = (Vector2)target.transform.position - rb.position;
+        direction.Normalize();
+        float rotateAmount = Vector3.Cross(direction, transform.up).z;
+        rb.angularVelocity = -rotateAmount * rotateSpeed;
+        rb.velocity = transform.up * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("bullet"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
